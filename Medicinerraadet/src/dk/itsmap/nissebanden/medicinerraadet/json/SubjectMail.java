94e780205ca5eeb6e81e72363e9bc29115c2734f
chr.smidtjensen@gmail.com <chr.smidtjensen@gmail.com@958a1de4-b4e4-d622-8e94-ed49cd850c34>
2013-05-28 10:03:19 +0000

    json or mails added

---
+++ Medicinerraadet/src/dk/itsmap/nissebanden/medicinerraadet/json/SubjectMail.java
@@ -0,0 +1,58 @@
+package dk.itsmap.nissebanden.medicinerraadet.json;
+
+public class SubjectMail {
+	private String id;
+	private String mailAdr;
+	private String nameFirst;
+	private String nameLast;
+	private String nickName;
+	private boolean showSemester;
+
+	public String getId() {
+		return this.id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getMailAdr() {
+		return this.mailAdr;
+	}
+
+	public void setMailAdr(String mailAdr) {
+		this.mailAdr = mailAdr;
+	}
+
+	public String getNameFirst() {
+		return this.nameFirst;
+	}
+
+	public void setNameFirst(String nameFirst) {
+		this.nameFirst = nameFirst;
+	}
+
+	public String getNameLast() {
+		return this.nameLast;
+	}
+
+	public void setNameLast(String nameLast) {
+		this.nameLast = nameLast;
+	}
+
+	public String getNickName() {
+		return this.nickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	public boolean getShowSemester() {
+		return this.showSemester;
+	}
+
+	public void setShowSemester(boolean showSemester) {
+		this.showSemester = showSemester;
+	}
+}
---
+++ Medicinerraadet/src/dk/itsmap/nissebanden/medicinerraadet/json/SubjectMails.java
@@ -0,0 +1,15 @@
+
+package dk.itsmap.nissebanden.medicinerraadet.json;
+
+import java.util.List;
+
+public class SubjectMails{
+   	private List mails;
+
+ 	public List getMails(){
+		return this.mails;
+	}
+	public void setMails(List mails){
+		this.mails = mails;
+	}
+}
