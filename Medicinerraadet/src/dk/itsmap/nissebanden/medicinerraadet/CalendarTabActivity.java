98ed595ff34ca9e4be514af0b972537b0557e48c
mariehoeg@gmail.com <mariehoeg@gmail.com@958a1de4-b4e4-d622-8e94-ed49cd850c34>
2013-05-30 15:13:26 +0000

    Kalender touch up

--- Medicinerraadet/src/dk/itsmap/nissebanden/medicinerraadet/CalendarTabActivity.java
+++ Medicinerraadet/src/dk/itsmap/nissebanden/medicinerraadet/CalendarTabActivity.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.http.HttpEntity;
@@ -28,12 +31,13 @@ import android.widget.TextView;
 
 public class CalendarTabActivity extends Activity {
 	WebView myWebView;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_calendar_tab);
 		myWebView = (WebView) findViewById(R.id.webView_Calendar);
-		Log.e("hej", myWebView+"");
+		Log.e("hej", myWebView + "");
 	}
 
 	@Override
@@ -42,10 +46,9 @@ public class CalendarTabActivity extends Activity {
 		getMenuInflater().inflate(R.menu.news_tab, menu);
 		return true;
 	}
-	
+
 	@Override
-	public void onStart()
-	{
+	public void onStart() {
 		super.onStart();
 		startDownloadMetode();
 	}
@@ -84,7 +87,8 @@ public class CalendarTabActivity extends Activity {
 			InputStream source = retrieveStream(url);
 			Gson gson = new Gson();
 			Reader reader = new InputStreamReader(source);
-			GC_GoogleCalendar RawSTOG = gson.fromJson(reader, GC_GoogleCalendar.class);
+			GC_GoogleCalendar RawSTOG = gson.fromJson(reader,
+					GC_GoogleCalendar.class);
 			return RawSTOG;
 		}
 
@@ -120,126 +124,129 @@ public class CalendarTabActivity extends Activity {
 			// setting downloaded into image view
 			// my_image.setImageDrawable(Drawable.createFromPath(imagePath));
 
-			
 			Log.e("HER_AsyncTask: ", "onPostExecute");
-			String css = ""+
-			
-			"<style type=\"text/css\">"+
+			String css = "" +
 
+			"<style type=\"text/css\">" +
 
-			"body {"+
-			    "background-color: #DAE0F0;"+
-			    "font-family: Arial, sans-serif;"+
-			    "font-size: 100%;"+
-			"}"+
+			"body {" + "background-color: #DAE0F0;"
+					+ "font-family: Arial, sans-serif;" + "font-size: 100%;"
+					+ "}" +
 
-			".header {"+
-			   "background-color: #9898B8;"+
-			   "margin-bottom: 0.5em;"+
-			   "border-bottom: 0.15em solid #2E3875;"+
-			"}"+
+					".header {" + "background-color: #9898B8;"
+					+ "margin-bottom: 0.5em;"
+					+ "border-bottom: 0.15em solid #2E3875;" + "}" +
 
-			".logo {"+
-			    "height: 2em;"+
-			    "float: left;"+
-			"}"+
-			    
-			".date {"+
-				"font-weight: bold;"+
-				"color: #FFFFFF;"+
-				"text-align: left;"+
-			"}"+
+					".logo {" + "height: 2em;" + "float: left;" + "}" +
 
-			"table {"+
-			    "width: 100%;"+
-			    "border-collapse: collapse;"+
-			    "border-spacing: 0;"+
-			"}"+
+					".date {" + "font-weight: bold;" + "color: #FFFFFF;"
+					+ "text-align: left;" + "}" +
 
-			    "table td {"+
-			        "vertical-align: central;"+
-			        "padding: 0;"+
-			    "}"+
+					"table {" + "width: 100%;" + "border-collapse: collapse;"
+					+ "border-spacing: 0;" + "}" +
 
-			".content {"+
-			    "text-overflow: ellipsis;"+
-			    "line-height: 1.2em;"+
-			    "max-height: 9.6em;"+
-			    "overflow: hidden;"+
-			"}"+
-			    
-    		".content a:link {"+
-		        "color: #2E3875;"+
-		    "}"+
-		        
-    		".content a:visited {"+
-		        "color: #2E3875;"+
-		    "}"+
+					"table td {" + "vertical-align: central;" + "padding: 0;"
+					+ "}" +
 
-			".img {"+
-			    "float: right;"+
-			    "margin: 0.2em;"+
-			    "max-width: 7.5em;"+
-			    "border-style: solid;"+
-			"}"+
+					"table td:nth-of-type(odd) {" + "width: 10%;" + "}" +
 
-			
+					".content {" + "text-overflow: ellipsis;"
+					+ "line-height: 1.2em;" + "max-height: 9.6em;"
+					+ "overflow: hidden;" + "}" +
 
-				"</style>";
-					
-					
-			String html1 = "<html lang=\"en\" xmlns=\"http://www.w3.org/1999/xhtml\">"+
-					"<head>"+
-						"<title>Newsfeed for MedHauze</title>"+
-						//"<link href=\"file:///android_asset/newscss.css\" rel=\"stylesheet\" />"+
-					"</head>"+
-					"<body>";
-		
+					".content a:link {" + "color: #2E3875;" + "}" +
+
+					".content a:visited {" + "color: #2E3875;" + "}" +
+
+					".img {" + "float: right;" + "margin: 0.2em;"
+					+ "max-width: 7.5em;" + "border-style: solid;" + "}"
+					+ "</style>";
+
+			String html1 = "<html lang=\"en\" xmlns=\"http://www.w3.org/1999/xhtml\">"
+					+ "<head>" + "<title>Newsfeed for MedHauze</title>" +
+					// "<link href=\"file:///android_asset/newscss.css\" rel=\"stylesheet\" />"+
+					"</head>" + "<body>";
 
 			String html_part = "";
 
 			List<GC_Entry> entrylist = result.getFeed().getEntry();
-			
-			for(GC_Entry entry : entrylist)
-			{
+
+			for (GC_Entry entry : entrylist) {
+				String startTime, headerTime, endTime, tempText, shortStartTime;
+				headerTime = "";
+				tempText = "";
+				SimpleDateFormat formatter, FORMATTER, HeaderFormatter;
+							
+				for (GD_When gd : entry.getGd_when()) {
+					startTime = gd.getStartTime();
+					endTime = gd.getEndTime();
+					if (startTime.length() > 10) {
+						shortStartTime = startTime.substring(0, 15);
+						endTime = endTime.substring(11, 16);
+						formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+						FORMATTER = new SimpleDateFormat("'D.' dd. MMMM yyyy 'kl.' HH:mm");
+						HeaderFormatter = new SimpleDateFormat("dd. MMMM yyyy");	
+						try {
+							startTime = FORMATTER.format(formatter.parse(shortStartTime));
+							headerTime = HeaderFormatter.format(formatter.parse(shortStartTime));
+							tempText = startTime+"-"+endTime+"<br>";
+							//temp += FORMATTER.format(formatter.parse(startTime))+ "-"+ endTime + " <br>";
+						} catch (ParseException e) {
+							e.printStackTrace();
+						}
+					} else {
+						formatter = new SimpleDateFormat("yyyy-MM-dd");
+						FORMATTER = new SimpleDateFormat("dd. MMMM yyyy");
+						try {
+							startTime = FORMATTER.format(formatter.parse(startTime));
+							headerTime = startTime;
+							endTime = FORMATTER.format(formatter.parse(endTime));
+							tempText = "Hel-/flerdagsarrangement <br>" 
+										+"Fra "+startTime
+										+" til "+endTime+"<br>";
+//							temp += "Fra "
+//									+ FORMATTER.format(formatter.parse(startTime))
+//									+ " til "
+//									+ FORMATTER.format(formatter.parse(endTime))
+//									+ "<br>";
+						} catch (ParseException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+				String temp = "<div class=\"header\">"
+						+ "<table>"
+						+ "<tr>"
+						+ "<td>"
+						+ "<img class=\"logo\" src=\"logomedicinerraadet.jpg\" />"
+						+ "</td>" + "<td>" + "<div class=\"date\">"
+						+ headerTime+ "</div>" + "</td>"
+						+ "</tr>" + "</table>" + "</div>"
+						+ "<div class=\"content\">"
+						+ "<b>"+entry.getTitle().get$t()+"</b><br>"
+						+ tempText;
+				
+				for (GD_Where gd : entry.getGd_where()) {
+					if (!gd.getValueString().isEmpty())
+						temp += gd.getValueString() + " <br><br>";
+				}
 				
 				
-				String temp ="<div class=\"header\">"+
-							"<table>"+
-	            				"<tr>"+
-	                				"<td>"+
-	                    				"<img class=\"logo\" src=\"logomedicinerraadet.jpg\" />"+
-	                    			"</td>"+
-	                    			"<td>"+
-	                    				"<div class=\"date\">"+entry.getTitle().get$t()+"</div>"+
-	                    			"</td>"+
-	                    		"</tr>"+
-	                    	"</table>"+
-	                    "</div>"+
-	                    "<div class=\"content\">";
-						for(GD_Where gd : entry.getGd_where())
-						{
-							temp+=gd.getValueString() + " <br>";
-						}
-						for(GD_When gd : entry.getGd_when())
-						{
-							temp+=gd.getStartTime() + " <br>";
-						}
-						temp+="</div>";	
-						
-	                    
-				html_part+=temp;
+				temp += "</div>";
+
+				html_part += temp;
 			}
 
-		     String html2 = "</body>"+
-			            "</html>";
-				//Log.e("HEHEH", css+html1+html_part+html2);
-				//myWebView.loadData(css+html1+html_part+html2, "text/html", "utf-8");
-				myWebView.loadDataWithBaseURL("file:///android_res/drawable/",css+html1+html_part+html2 , "text/html", "utf-8",null);
+			String html2 = "</body>" + "</html>";
+			// Log.e("HEHEH", css+html1+html_part+html2);
+			// myWebView.loadData(css+html1+html_part+html2, "text/html",
+			// "utf-8");
+			myWebView.loadDataWithBaseURL("file:///android_res/drawable/", css
+					+ html1 + html_part + html2, "text/html", "utf-8", null);
 
 		}
 	}
-	
+
 	public InputStream retrieveStream(String url) {
 		DefaultHttpClient client = new DefaultHttpClient();
 		HttpGet getRequest = new HttpGet(url);
@@ -259,21 +266,5 @@ public class CalendarTabActivity extends Activity {
 		}
 		return null;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
